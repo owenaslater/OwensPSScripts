51c4c50a71b1daf55908b7797868ad3a65a2459c
OwenASlater <owenslater@hotmail.com>
2024-04-15 11:31:58 +0100

    LAFS - App name remove and add trycatch

--- LeftoverAppFilesScan.ps1
+++ LeftoverAppFilesScan.ps1
@@ -1,4 +1,4 @@
-﻿$appName = "Dell"
+﻿$appName = ""
 $scanDirectories = @("Program Files","Program Files (x86)", "users\*\AppData", "ProgramData")
 
 foreach($directory in $scanDirectories){
@@ -7,20 +7,24 @@ Get-ChildItem -Directory -Path "$env:HomeDrive\$directory" -Recurse | where {$_.
 }
 
 $userRegistryDirectories = Get-ChildItem -Path Registry::HKEY_USERS | where {([string]$_.Name.Length -gt 20) -and ([string]$_.Name -notlike "*_Classes")}
-
+ 
 $RegistryKeys = @($userRegistryDirectories.Name, "HKEY_LOCAL_MACHINE")
 $RegistryAppPaths = @("SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall","SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall")
 foreach($directory in $RegistryKeys){
     $currentDirectory=$directory
     foreach($path in $RegistryAppPaths){
         Write-Host "Scanning Registry $directory\$path" -ForegroundColor Yellow
-        $value = Get-ChildItem -Path "Registry::$directory\$path" | Get-ItemProperty | where {$_.DisplayName -like "*$appName*"}
-        if(-not[string]::IsNullOrEmpty($value)){
-            foreach($app in $value){
-                $valueName = $app.DisplayName
-                $valuePath = $app.PSChildName
-                Write-Host "$valueName in key $valuePath" -ForegroundColor Cyan
+        try {
+            $value = Get-ChildItem -Path "Registry::$currentdirectory\$path" -ErrorAction Ignore | Get-ItemProperty | where {$_.DisplayName -like "*$appName*"}
+            if(-not[string]::IsNullOrEmpty($value)){
+                foreach($app in $value){
+                    $valueName = $app.DisplayName
+                    $valuePath = $app.PSChildName
+                    Write-Output "$valueName in key $valuePath" -ForegroundColor Cyan
+                }
             }
+        } catch {
+            Write-Host "$_" -ForegroundColor Red
         }
     }
 }
